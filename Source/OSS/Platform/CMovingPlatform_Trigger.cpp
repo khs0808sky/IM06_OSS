7a5df27501788938fdf0f8e3b8d397480b857c18
msk0000 <khs0808sky@naver.com>
2024-12-19 19:51:58 +0900

    1219_MovingPlatform

---
+++ Source/OSS/Platform/CMovingPlatform_Trigger.cpp
@@ -0,0 +1,35 @@
+#include "CMovingPlatform_Trigger.h"
+#include "Components/BoxComponent.h"
+#include "CMovingPlatform.h"
+
+ACMovingPlatform_Trigger::ACMovingPlatform_Trigger()
+{
+	BoxComp = CreateDefaultSubobject<UBoxComponent>("BoxComp");
+	RootComponent = BoxComp;
+}
+
+void ACMovingPlatform_Trigger::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	OnActorBeginOverlap.AddDynamic(this, &ACMovingPlatform_Trigger::ActorBeginOverlap);
+	OnActorEndOverlap.AddDynamic(this, &ACMovingPlatform_Trigger::ActorEndOverlap);
+}
+
+void ACMovingPlatform_Trigger::ActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	for (const auto& Platform : PlatformsToTrigger)
+	{
+		Platform->IncreaseActiveCount();
+	}
+}
+
+void ACMovingPlatform_Trigger::ActorEndOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	for (const auto& Platform : PlatformsToTrigger)
+	{
+		Platform->DecreaseActiveCount();
+	}
+}
+
+
