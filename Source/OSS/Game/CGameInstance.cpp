8fa46ed2fa39de369bff7b28d97d9f398ea06568
msk0000 <khs0808sky@naver.com>
2024-12-24 19:50:39 +0900

    1224_PauseMenu

--- Source/OSS/Game/CGameInstance.cpp
+++ Source/OSS/Game/CGameInstance.cpp
@@ -1,5 +1,6 @@
 #include "CGameInstance.h"
 #include "UI/CMainMenu.h"
+#include "UI/CPauseMenu.h"
 
 UCGameInstance::UCGameInstance()
 {
@@ -8,6 +9,12 @@ UCGameInstance::UCGameInstance()
 	if (MainMenuClassAsset.Succeeded())
 	{
 		MainMenuClass = MainMenuClassAsset.Class;
+	}
+
+	ConstructorHelpers::FClassFinder<UUserWidget> PauseMenuClassAsset(TEXT("/Game/UI/WB_PauseMenu"));
+	if (PauseMenuClassAsset.Succeeded())
+	{
+		PauseMenuClass = PauseMenuClassAsset.Class;
 	}
 }
 
@@ -34,6 +41,11 @@ void UCGameInstance::Host()
 
 void UCGameInstance::Join(const FString& InAddress)
 {
+	if (MainMenu)
+	{
+		MainMenu->Shutdown();
+	}
+
 	UEngine* Engine = GetEngine();
 	if (!Engine) return;
 	Engine->AddOnScreenDebugMessage(-1, 10.f, FColor::Green, FString::Printf(TEXT("Join to %s"), *InAddress));
@@ -41,6 +53,13 @@ void UCGameInstance::Join(const FString& InAddress)
 	APlayerController* PC = GetFirstLocalPlayerController();
 	if (!PC) return;
 	PC->ClientTravel(InAddress, TRAVEL_Absolute);
+}
+
+void UCGameInstance::OpenMainMenuLevel()
+{
+	APlayerController* PC = GetFirstLocalPlayerController();
+	if (!PC) return;
+	PC->ClientTravel("/Game/Maps/MainMenu", TRAVEL_Absolute);
 }
 
 void UCGameInstance::LoadMainMenu()
@@ -53,4 +72,16 @@ void UCGameInstance::LoadMainMenu()
 	MainMenu->SetOwningInstance(this);
 
 	MainMenu->Startup();
+}
+
+void UCGameInstance::LoadPauseMenu()
+{
+	if (!PauseMenuClass) return;
+
+	UCPauseMenu* PauseMenu = CreateWidget<UCPauseMenu>(this, PauseMenuClass);
+	if (!PauseMenu) return;
+
+	PauseMenu->SetOwningInstance(this);
+
+	PauseMenu->Startup();
 }
